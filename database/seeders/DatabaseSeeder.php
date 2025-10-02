4e0f5e918dda8a52438a3c2779f3cbd1e929177c
DO QUANG VINH <doquangving@gmail.com>
2025-10-02 22:04:26 +0700

    bugfix: Fix migrations and seeders.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,5 +3,5 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 class DatabaseSeeder extends Seeder
 {
-    public function run(): void { $this->call([LocationSeeder::class, UserSeeder::class, CatalogSeeder::class, ContentSeeder::class, CouponSeeder::class]); }
+    public function run(): void { $this->call([CatalogSeeder::class, ContentSeeder::class, CouponSeeder::class]); }
 }
